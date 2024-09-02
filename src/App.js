92d66a14ccb0c4e8fecd71c160ccf6187423971f
Dhiya Joseph <dhiyajsph@gmail.com>
2024-09-02 15:33:39 +0530

    Add components folder and other untracked files

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
 import './App.css';
-
+import Navbar from './components/Navbar'
+import Myself from './components/Myself'
+import About from './components/About'
+import Interests from './components/Interests'
+import Projects from './components/Projects'
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div >
+      <Navbar />
+      <Myself/>
+      <About />
+      <Interests />
+      <Projects/>
+      
     </div>
   );
 }
 
 export default App;
+
